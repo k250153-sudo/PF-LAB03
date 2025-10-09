d78bf348b92051ec4b3fbe58dd0627214cf21199
Muhammad Mudassir <k250153@nu.edu.pk>
2025-10-10 01:15:33 +0500

    Add files via upload

---
+++ Lab06/Q10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j < n; j++) printf(" ");
+        for (int j = 1; j <= (2 * i - 1); j++) {
+            if (j == 1 || j == (2 * i - 1)) printf("*");
+            else printf(" ");
+        }
+        printf("\n");
+    }
+
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = n; j > i; j--) printf(" ");
+        for (int j = 1; j <= (2 * i - 1); j++) {
+            if (j == 1 || j == (2 * i - 1)) printf("*");
+            else printf(" ");
+        }
+        printf("\n");
+    }
+}
